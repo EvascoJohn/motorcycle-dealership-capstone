300536a361e565fcf590701671012ff0c1a6bfff
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-13 23:22:08 +0800

    Name

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable implements FilamentUser
 
     public function getNameAttribute()
     {
-    return $this->first_name.' '.$this->last_name;
+    return $this->firstname.' '.$this->lastname;
     }
 
     
