73075365c46a187cc1c5deb482b1b1f8791bd31e
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-13 23:50:35 +0800

    chassis is nullable

--- database/migrations/2023_08_28_032605_create_units_table.php
+++ database/migrations/2023_08_28_032605_create_units_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->foreignIdFor(Branch::class)->nullable();
             $table->foreignIdFor(UnitModel::class);
             $table->string('chassis_number')
+                        ->nullable()
                     ->unique();
             $table->string('frame_number')
                     ->unique();
