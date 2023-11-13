7b7e911a3a1375f3acf4021505a2f66f2bbc3ba6
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-14 00:33:17 +0800

    Revert "chassis is nullable"
    This reverts commit 73075365c46a187cc1c5deb482b1b1f8791bd31e.

--- database/migrations/2023_08_28_032605_create_units_table.php
+++ database/migrations/2023_08_28_032605_create_units_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->foreignIdFor(Branch::class)->nullable();
             $table->foreignIdFor(UnitModel::class);
             $table->string('chassis_number')
-                        ->nullable()
                     ->unique();
             $table->string('frame_number')
                     ->unique();
