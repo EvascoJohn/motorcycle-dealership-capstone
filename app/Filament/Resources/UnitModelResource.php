40693ff4b45b85dfffaac6fc5e3472aa13572486
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-13 21:50:16 +0800

    money

--- app/Filament/Resources/UnitModelResource.php
+++ app/Filament/Resources/UnitModelResource.php
@@ -98,7 +98,7 @@ class UnitModelResource extends Resource
                     ->columnSpan(2),
                 Forms\Components\TextInput::make('price')
                     ->mask(Rawjs::make(<<<'JS'
-                        ('.', ',', 2)
+                        $money($input, '.', ',', 4)
                     JS))
                     ->required()
                     ->columnSpan(1),
