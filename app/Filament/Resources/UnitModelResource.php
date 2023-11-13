317ba3ae22c6518933bdb7cd2f5c40fcfe96c9fa
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-13 21:51:16 +0800

    decimal places on two

--- app/Filament/Resources/UnitModelResource.php
+++ app/Filament/Resources/UnitModelResource.php
@@ -98,7 +98,7 @@ class UnitModelResource extends Resource
                     ->columnSpan(2),
                 Forms\Components\TextInput::make('price')
                     ->mask(Rawjs::make(<<<'JS'
-                        $money($input, '.', ',', 4)
+                        $money($input, '.', ',', 2)
                     JS))
                     ->required()
                     ->columnSpan(1),
