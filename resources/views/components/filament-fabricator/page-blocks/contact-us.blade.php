a507d2cf4c7f1ffa1857dd9c392a2fec06d4734e
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-15 04:04:38 +0800

    initial contact_us

---
+++ resources/views/components/filament-fabricator/page-blocks/contact-us.blade.php
@@ -0,0 +1,37 @@
+@aware(['page'])
+<section id="Content3">
+    <div class="heading">
+      <h2 class="brandName">Brand Name</h2>
+      <p class="brandDescription">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In ipsum.
+      </p>
+    </div>
+    <div class="mission-container">
+      <div class="mission-image">
+        <img src="../images/Banner.jpg" alt="mission-image" />
+      </div>
+      <div class="mission-text">
+        <h2 class="mission">Mission</h2>
+        <p class="mission-content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+          placeat eius pariatur ipsam architecto optio aliquam? Iste quidem
+          placeat quod architecto, exercitationem minus, culpa optio dolorem
+          debitis voluptates sint fugiat.
+        </p>
+      </div>
+    </div>
+    <div class="vision-container">
+      <div class="vision-text">
+        <h2 class="vision">Vision</h2>
+        <p class="vision-content">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti
+          placeat eius pariatur ipsam architecto optio aliquam? Iste quidem
+          placeat quod architecto, exercitationem minus, culpa optio dolorem
+          debitis voluptates sint fugiat.
+        </p>
+      </div>
+      <div class="vision-image">
+        <img src="../images/Banner.jpg" alt="vision-image" />
+      </div>
+    </div>
+  </section>
