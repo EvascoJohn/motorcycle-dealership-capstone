f3020be9399749b27f0acedc21337a0a64e687a3
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-14 10:43:18 +0800

    new

--- app/Filament/Resources/UnitReleaseResource.php
+++ app/Filament/Resources/UnitReleaseResource.php
@@ -78,8 +78,7 @@ class UnitReleaseResource extends Resource
                                                 $preffered_unit_model = $record->unit_model_id;
                                                 $preffered_unit_status = $record->preffered_unit_status;
 
-                                                $unit_query = Models\Unit::query()->where(['id' => $preffered_unit_model],
-                                                                                            ['status' => $preffered_unit_status]);
+                                                $unit_query = Models\Unit::query()->where(['id' => $preffered_unit_model]);
 
                                                 dd($unit_query->pluck('chassis_number', "id")->toArray());
 
