d0f942ab1418d20a7c04860b89f9e601c8120753
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-14 10:05:02 +0800

    chat gpt

--- app/Filament/Resources/UnitReleaseResource.php
+++ app/Filament/Resources/UnitReleaseResource.php
@@ -90,15 +90,20 @@ class UnitReleaseResource extends Resource
                                             function (Forms\Get $get): array {
                                                 $units_query = Models\Unit::where('unit_model_id', $get('unit_model_id'))
                                                 ->where('status', $get('preffered_unit_status'));
-
-                                                if ($get('preffered_unit_status') == 'repo') {
+                                            
+                                            if ($get('preffered_unit_status') == 'repo') {
                                                 $units_query->where('customer_application_id', '!=', null);
-                                                } else if ($get('preffered_unit_status') == 'brand_new') {
+                                            } else if ($get('preffered_unit_status') == 'brand_new') {
                                                 $units_query->where('customer_application_id', null);
-                                                }
-
-                                                // dd($units_query->pluck('chassis_number', 'id')->toArray());
-                                                return $units_query->pluck('chassis_number', 'id')->toArray() ?? [ "" => ""];
+                                            }
+                                            
+                                            $result = $units_query->pluck('chassis_number', 'id')->toArray();
+                                            $result = array_filter($result, function ($value) {
+                                                return $value !== null;
+                                            });
+                                            
+                                            // dd($result);
+                                            return $result;
                                             }
                                     )
                                     ->afterStateUpdated(
