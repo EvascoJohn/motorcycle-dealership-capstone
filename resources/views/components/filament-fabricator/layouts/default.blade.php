5fb7bbf263732eb005f3e8b04582c3ef476d88e9
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-15 19:29:24 +0800

    renamed the default  title

--- resources/views/components/filament-fabricator/layouts/default.blade.php
+++ resources/views/components/filament-fabricator/layouts/default.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{ asset('css/own/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/own/media-queries.css') }}" />
     {{-- <link rel="stylesheet" href="{{ asset('css/own/product-page-specs.css') }}" /> --}}
-    <title>Document</title>
+    <title>Motorstar - Probike Motorcycle Center</title>
     <link
     rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
