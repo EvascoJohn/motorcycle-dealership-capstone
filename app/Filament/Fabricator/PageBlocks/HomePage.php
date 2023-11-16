47c77a3f468953af569fbf6cf0222db13a6816f5
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-16 10:53:06 +0800

    test

--- app/Filament/Fabricator/PageBlocks/HomePage.php
+++ app/Filament/Fabricator/PageBlocks/HomePage.php
@@ -49,8 +49,7 @@ class HomePage extends PageBlock
     public static function mutateData(array $data): array
     {
         $data['company_name'] = env('APP_NAME');
-        $data['product_list'] = UnitModel::query()->take(3);
-        dd($data['products_list']);
+        $data['product_list'] = UnitModel::query()->all()->take(3);
         return $data;
     }
 }
