02723e851478aabb1dfe9838999c711b192457b8
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-16 10:41:45 +0800

    unit model images

--- app/Filament/Resources/UnitModelResource.php
+++ app/Filament/Resources/UnitModelResource.php
@@ -87,11 +87,13 @@ class UnitModelResource extends Resource
     public static function getImportantDetailsComponent(): Forms\Components\Component
     {
         return Forms\Components\Group::make([
-                Forms\Components\FileUpload::make('image_file')
-                    ->columnSpan(3)
-                    ->directory('unit_model_images')
-                    ->acceptedFileTypes(['image/png','image/jpg'])
-                    ->disk('public'),
+            Forms\Components\FileUpload::make("image_file")
+                        ->required()
+                        ->label("Heading Image")
+                        ->disk('public')
+                        ->directory('unit_model_images')
+                        ->visibility('private')
+                        ->acceptedFileTypes(['image/jpeg']),
                 Forms\Components\TextInput::make('model_name')
                     ->maxLength(255)
                     ->required()
