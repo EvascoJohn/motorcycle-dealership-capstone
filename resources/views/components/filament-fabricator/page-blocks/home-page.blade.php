d7e445a78e28fda007c38d345cf3f24df0fe8c08
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-16 09:33:53 +0800

    added contents on home page

--- resources/views/components/filament-fabricator/page-blocks/home-page.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/home-page.blade.php
@@ -8,6 +8,40 @@
     'requirements',
 ])
 
+<header>
+  <!-- Brand logo Text -->
+  <a href="#" class="logo">{{ $company_name }}</a>
+  <!-- Navigation bar -->
+  <ul class="navbar">
+      {{-- redirects to the home --}}
+      <li>
+          <a href="/home" class="{{ request()->is('home') ? 'active': 'hidden' }}">Home</a>
+      </li>
+      {{-- redirects to the products --}}
+      <li>
+          <a href="/products" class="{{ request()->is('products') ? 'active': 'hidden' }}">Products</a>
+      </li>
+      <li>
+          @auth<a href="/application" class="{{ request()->is('application') ? 'active': 'hidden' }}">Application</a>@endauth
+      </li>
+      {{-- redirects to the about --}}
+      <li>
+          <a href="/about-us" class="{{ request()->is('about-us') ? 'active': 'hidden' }}">About Us</a>
+      </li>
+      {{-- redirects to the contact --}}
+      <li>
+          <a href="/contact-us" class="{{ request()->is('contact-us') ? 'active': 'hidden' }}">Contact Us</a>
+      </li>
+  </ul>
+  <!-- User registration and menu icon -->
+  <div class="main">
+      <a href="/customer" class="user" id="loginLink">Log In 
+          <i class="ri-account-circle-fill"></i>
+      </a>
+      <div class="ri-menu-line" id="menu-icon"></div>
+  </div>
+</header>
+
 <section id="Content1">
     <div>
        <!-- Heading Image2 -->
@@ -26,4 +60,22 @@
         </div>
       </div>
     </div>
+</section>
+
+
+<section id="Content3">
+  <div class="heading">
+    <h1 class="Title"> Introducing the Latest Arrival to Our Motorcycle Lineup! </h1>
+  </div>
+  <div class="home-product-row"> @foreach ($product_list as $product) 
+    <div class="home-product-col">
+      <img src="{{ "storage/app/public/".$product->image_file }}" alt="" />
+      <h2 class="testi-name">{{ $product->model_name }}</h2>
+      <div class="c3-buttons">
+        <a href="/products/product-specs/{{$product->id}}">View Full Specs</a>
+      </div>
+    </div> @endforeach </div>
+  <div class="c3-buttons">
+    <a href="../html/products.html">See More</a>
+  </div>
 </section>
