104530d9d6afbf61c51812071b5ad717bf776c1c
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-18 15:20:21 +0800

    swapped id to full name

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -124,8 +124,8 @@ class PaymentResource extends Resource
                 Forms\Components\Select::make('customer_application_id')
                 ->searchable()
                 ->columnSpan(1)
-                ->getSearchResultsUsing(fn (string $search): array => CustomerApplication::getSearchApplicationsReadyForPayment($search)->get()->pluck("id", "applicant_full_name")->toArray())
-                ->getOptionLabelUsing(fn ($value): ?string => CustomerApplication::find($value)->applicant_full_name)
+                ->getSearchResultsUsing(fn (string $search): array => CustomerApplication::getSearchApplicationsReadyForPayment($search)->get()->pluck("applicant_full_name", "id")->toArray())
+                ->getOptionLabelUsing(fn ($value): ?string => CustomerApplication::find($value)->id)
                 ->required()
                 ->live()
                 ->afterStateUpdated(
@@ -133,7 +133,7 @@ class PaymentResource extends Resource
                         $application = CustomerApplication::query()
                                 ->where("id", $state)
                                 ->first();
-                        dd($state);
+                        dd($sate);
                         $set('due_date', "");
                         $set('payment_amount', "");
                         $set('application_firstname',  "");
