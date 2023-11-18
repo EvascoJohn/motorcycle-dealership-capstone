65f208a10643e5e9032619c7cb2d0ca1d552b67d
evasco223692 <evasco.223692@balayan.sti.edu.ph>
2023-11-18 15:02:49 +0800

    with id line: 127

--- app/Filament/Resources/PaymentResource.php
+++ app/Filament/Resources/PaymentResource.php
@@ -124,7 +124,7 @@ class PaymentResource extends Resource
                 Forms\Components\Select::make('customer_application_id')
                 ->searchable()
                 ->columnSpan(1)
-                ->getSearchResultsUsing(fn (string $search): array => CustomerApplication::getSearchApplicationsReadyForPayment($search)->get()->pluck("id", "applicant_full_name")->toArray())
+                ->getSearchResultsUsing(fn (string $search): array => CustomerApplication::getSearchApplicationsReadyForPayment($search)->get()->pluck("id", "applicant_full_name_with_iw")->toArray())
                 ->getOptionLabelUsing(fn ($value): ?string => CustomerApplication::find($value)->applicant_full_name_with_iw)
                 ->required()
                 ->live()
